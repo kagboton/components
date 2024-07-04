73f3dfdbcd59b7e6bff33cacfe0fa12627fd73d4
Kilian Agboton <agbotonstefane@gmail.com>
2024-07-03 22:15:39 -0400

    created modules to organize components

---
+++ src/app/elements/elements-home/elements-home.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elements-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './elements-home.component.html',
+  styleUrl: './elements-home.component.css',
+})
+export class ElementsHomeComponent {}
---
+++ src/app/suimodules/suimodules.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SUIModulesRoutingModule } from './suimodules-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SUIModulesRoutingModule
+  ]
+})
+export class SUIModulesModule { }

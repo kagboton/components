b291aef148eb07118c046d8114d7dfee07296000
Kilian Agboton <agbotonstefane@gmail.com>
2024-07-05 01:02:32 -0400

    components lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ElementsModule } from './elements/elements.module';
-import { CollectionsModule } from './collections/collections.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +8,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, ElementsModule, CollectionsModule, AppRoutingModule], // order matters
+  imports: [BrowserModule, AppRoutingModule], // order matters
   providers: [],
   bootstrap: [AppComponent],
 })
